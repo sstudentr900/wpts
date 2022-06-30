957a4ad66448516587bdfea9ae022d6b722748c0
sstudentr123 <sstudentr123700900@yahoo.com.tw>
2022-06-30 11:28:43 +0800

    init

---
+++ vendor/composer/installed.php
@@ -0,0 +1,23 @@
+<?php return array(
+    'root' => array(
+        'pretty_version' => 'dev-main',
+        'version' => 'dev-main',
+        'type' => 'library',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'reference' => '7a134a649c73d2a6d533dea1728cb547cb51e57c',
+        'name' => '__root__',
+        'dev' => true,
+    ),
+    'versions' => array(
+        '__root__' => array(
+            'pretty_version' => 'dev-main',
+            'version' => 'dev-main',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'reference' => '7a134a649c73d2a6d533dea1728cb547cb51e57c',
+            'dev_requirement' => false,
+        ),
+    ),
+);
